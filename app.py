5a1f54b5c2e7dadf49da219002da7866c0ce903e
ASDawn69 <79701238+ASDawn69@users.noreply.github.com>
2025-07-22 14:31:04 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+
+# CSV読み込み
+df = pd.read_csv("directory_list_utf8.csv",encoding="utf-8")
+
+# TF-IDF初期化
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(df["カテゴリ名"])
+
+# Flaskアプリ初期化
+app = Flask(__name__)
+
+@app.route("/match", methods=["POST"])
+def match():
+    data = request.get_json()
+    keyword = data.get("keyword", "")
+    input_vec = vectorizer.transform([keyword])
+    similarities = cosine_similarity(input_vec, tfidf_matrix)
+    best_index = similarities.argmax()
+    matched_id = df.iloc[best_index]["ディレクトリID"]
+    return jsonify({"directory_id": matched_id})
+
+if __name__ == "__main__":
+    app.run()
